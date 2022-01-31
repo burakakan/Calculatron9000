b283fe45b3eedfc72b5157e984906f1183652e87
burakakan <98014644+burakakan@users.noreply.github.com>
2022-01-31 15:27:54 +0300

    Minor changes.

---
+++ Calculatron9000/MathExp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatron9000
+{
+    static class MathExp
+    {
+        enum Ops { plus, times, ln };
+
+        static IDictionary<string, Ops> OpDic => new Dictionary<string, Ops>() {
+                {"+", Ops.plus},
+                {"*", Ops.times},
+                {"ln", Ops.ln},
+            };
+
+        public static List<object> Arrange(this String exp)
+        {
+            List<object> elements = new List<object>();
+
+            //clear whitespaces
+            exp.Replace(" ", "");
+
+            //isolate adjacent operators with whitespaces
+            foreach (string o in OpDic.Keys.ToArray())
+                exp = exp.Replace(o, " " + o + " ");
+
+            //split the expression by the operators to get the numbers
+            //seperators are operators with attached whitespaces
+            string[] nums = exp.Split(OpDic.Keys.ToArray().Select(o => " " + o + " ").ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            string[] expParsed = exp.Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string e in expParsed)
+            {
+                if (nums.Contains(e))
+                    elements.Add(Convert.ToDouble(e));
+                else
+                    elements.Add(OpDic[e]);
+            }
+
+            return elements;
+        }
+    }
+}
